ad800a23ad55432386701778383934c37bd33f94
Maya Peretz <mperetz@redhat.com>
2024-07-24 14:59:17 +0300

    Added ecsAutoScalingRole role

--- terraform/main.tf
+++ terraform/main.tf
@@ -79,3 +79,25 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy_attach
   role       = aws_iam_role.ecs_task_execution_role.name
   policy_arn = "${data.aws_iam_policy.ecs_task_execution_role_policy.arn}"
 }
+
+
+resource "aws_iam_role" "ecs_auto_scailing_role" {
+  name = "ecsAutoScalingRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        },
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_auto_scailing_attachment" {
+  role       = aws_iam_role.ecs_auto_scailing_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceAutoscaleRole"
+}
