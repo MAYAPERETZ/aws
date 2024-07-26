918dc8dea08698a855b336d7f02c45b9dab54e2d
Maya Peretz <mperetz@redhat.com>
2024-07-26 14:55:43 +0300

    Deployed ECS using Fargate

--- terraform/main.tf
+++ terraform/main.tf
@@ -130,7 +130,7 @@ module "vpc" {
   public_subnets  = ["172.16.1.0/24", "172.16.0.0/24"]
 
   create_igw = true
-
+  map_public_ip_on_launch = true
   tags = {
     Terraform = "true"
     Environment = "dev"
@@ -138,26 +138,25 @@ module "vpc" {
 }
 
 
-module "ecs_ec2_cluster" {
+module "ecs_fargate_cluster" {
   source = "terraform-aws-modules/ecs/aws"
 
   cluster_name = local.name
 
   # Capacity provider - autoscaling groups
-  default_capacity_provider_use_fargate = false
-  autoscaling_capacity_providers = {
-    # On-demand instances
-    ex_1 = {
-      auto_scaling_group_arn         = module.autoscaling["ex_1"].autoscaling_group_arn
-      managed_termination_protection = "ENABLED"
+  default_capacity_provider_use_fargate = true
+#   autoscaling_capacity_providers = {
+#     # On-demand instances
+#     ex_1 = {
+#       auto_scaling_group_arn         = module.autoscaling["ex_1"].autoscaling_group_arn
 
-      managed_scaling = {
-        maximum_scaling_step_size = 2
-        minimum_scaling_step_size = 1
-        status                    = "ENABLED"
-      }
-    }
-  }
+#       managed_scaling = {
+#         maximum_scaling_step_size = 2
+#         minimum_scaling_step_size = 1
+#         status                    = "ENABLED"
+#       }
+#     }
+#   }
 
   tags = local.tags
 }
@@ -172,9 +171,9 @@ module "ecs_ec2_cluster" {
 ################################################################################
 
 # https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html#ecs-optimized-ami-linux
-data "aws_ssm_parameter" "ecs_optimized_ami" {
-  name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
-}
+# data "aws_ssm_parameter" "ecs_optimized_ami" {
+#   name = "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended"
+# }
 
 # module "alb" {
 #   source  = "terraform-aws-modules/alb/aws"
@@ -245,62 +244,55 @@ data "aws_ssm_parameter" "ecs_optimized_ami" {
 
 # }
 
-module "autoscaling" {
-  source  = "terraform-aws-modules/autoscaling/aws"
+# module "autoscaling" {
+#   source  = "terraform-aws-modules/autoscaling/aws"
 
-  min_size            = 1
-  max_size            = 2
-  desired_capacity    = 1
+#   min_size            = 1
+#   max_size            = 2
+#   desired_capacity    = 1
 
-  for_each = {
-    # On-demand instances
-    ex_1 = {
-      instance_type              = "t3.micro"
-      use_mixed_instances_policy = false
-      mixed_instances_policy     = {}
-      user_data                  = <<-EOT
-        #!/bin/bash
+#   for_each = {
+#     # On-demand instances
+#     ex_1 = {
+#       instance_type              = "t2.micro"
+#       user_data                  = <<-EOT
+#         #!/bin/bash
+#         echo ECS_CLUSTER=${local.name} >> /etc/ecs/ecs.config;
+#       EOT
+#     }
+#   }
 
-        cat <<'EOF' >> /etc/ecs/ecs.config
-        ECS_CLUSTER=${local.name}
-        ECS_LOGLEVEL=debug
-        ECS_CONTAINER_INSTANCE_TAGS=${jsonencode(local.tags)}
-        ECS_ENABLE_TASK_IAM_ROLE=true
-        EOF
-      EOT
-    }
-  }
+#   name = "${local.name}-${each.key}"
 
-  name = "${local.name}-${each.key}"
+#   image_id      = jsondecode(data.aws_ssm_parameter.ecs_optimized_ami.value)["image_id"]
+#   instance_type = each.value.instance_type
 
-  image_id      = jsondecode(data.aws_ssm_parameter.ecs_optimized_ami.value)["image_id"]
-  instance_type = each.value.instance_type
+#   security_groups                 = [module.autoscaling_sg.security_group_id]
+#   user_data                       = base64encode(each.value.user_data)
+#   ignore_desired_capacity_changes = true
 
-  # security_groups                 = [module.autoscaling_sg.security_group_id]
-  user_data                       = base64encode(each.value.user_data)
-  ignore_desired_capacity_changes = true
+#   create_iam_instance_profile = true # Instance profile is the way you attach the role to the EC2 instance
+#   #iam_instance_profile_name   = aws_iam_role.ecs_instance_role.name
+#   iam_role_name               = local.name
+#   iam_role_description        = "ECS role for ${local.name}"
+#   iam_role_policies = {
+#     AmazonEC2ContainerServiceforEC2Role = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+#     AmazonSSMManagedInstanceCore        = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+#   }
 
-  create_iam_instance_profile = true # Instance profile is the way you attach the role to the EC2 instance
-  iam_role_name               = local.name
-  iam_role_description        = "ECS role for ${local.name}"
-  iam_role_policies = {
-    AmazonEC2ContainerServiceforEC2Role = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
-    AmazonSSMManagedInstanceCore        = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
-  }
+#   vpc_zone_identifier = module.vpc.public_subnets
+#   health_check_type   = "EC2"
 
-  vpc_zone_identifier = module.vpc.public_subnets
-  health_check_type   = "EC2"
+#   # https://github.com/hashicorp/terraform-provider-aws/issues/12582
+#   autoscaling_group_tags = {
+#     AmazonECSManaged = ""
+#   }
 
-  # https://github.com/hashicorp/terraform-provider-aws/issues/12582
-  autoscaling_group_tags = {
-    AmazonECSManaged = true
-  }
+#   # Required for  managed_termination_protection = "ENABLED"
+#   # protect_from_scale_in = true
 
-  # Required for  managed_termination_protection = "ENABLED"
-  protect_from_scale_in = true
-
-  tags = local.tags
-}
+#   tags = local.tags
+# }
 
 # module "autoscaling_sg" {
 #   source  = "terraform-aws-modules/security-group/aws"
@@ -310,15 +302,22 @@ module "autoscaling" {
 #   description = "Autoscaling group security group"
 #   vpc_id      = module.vpc.vpc_id
 
-#   computed_ingress_with_source_security_group_id = [
-#     {
-#       rule                     = "http-80-tcp"
-#       source_security_group_id = module.alb.security_group_id
-#     }
-#   ]
-#   number_of_computed_ingress_with_source_security_group_id = 1
+# #   computed_ingress_with_source_security_group_id = [
+# #     {
+# #       rule                     = "http-80-tcp"
+# #       source_security_group_id = module.alb.security_group_id
+# #     }
+# #   ]
+# #   number_of_computed_ingress_with_source_security_group_id = 1
 
 #   egress_rules = ["all-all"]
 
+#   ingress_with_self = [{
+#     protocol   = "-1" # -1 specifies all protocols
+#     from_port  = 0    # from_port and to_port are not required for all protocols
+#     to_port    = 0    # but set to 0 for clarity
+#     description = "Allow all traffic within the security group"
+#   }]
+  
 #   tags = local.tags
 # }
